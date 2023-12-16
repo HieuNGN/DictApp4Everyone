808b7a67a1b0715a70514602031083d69e4ea1d2
Hieu Ng N <129951778+HieuNGN@users.noreply.github.com>
2023-12-16 12:52:40 +0700

    update

--- src/main/java/com/example/dictionary/core/DictionaryB.java
+++
@@ -1,8 +0,0 @@
-package com.example.dictionary.core;
-
-import java.util.*;
-
-public class DictionaryB extends ArrayList<Words> {
-    // ArrayList of Words uses to store all words in the dictionary
-    public static List<Words> dictionary = new ArrayList<Words>();
-}
